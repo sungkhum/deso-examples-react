fe289bea039994d1e264cb6c7a91a22d7e3c7252
sungkhum <sungkhum@gmail.com>
2023-08-20 14:49:21 -0700

    Update nav.jsx
    Add the ability to have multiple users logged-in in a drop-down menu, display their profile image, add a new user as well as logout.

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -5,12 +5,18 @@ import { Link } from "react-router-dom";
 import { getDisplayName } from "../helpers";
 
 export const Nav = () => {
-  const { currentUser, isLoading } = useContext(DeSoIdentityContext);
+  const { currentUser, alternateUsers, isLoading } = useContext(DeSoIdentityContext);
+
+  const getProfilePictureUrl = (user) => {
+    return `https://diamondapp.com/api/v0/get-single-profile-picture/${user.PublicKeyBase58Check}?fallback=https://bitclout.com/assets/img/default_profile_pic.png`;
+  };
 
   return (
     <nav className="main-nav">
       <Link to="/">Home</Link>
+      <Link to="/">
       <Link to="/sign-and-submit-tx">Sign and Submit Transaction</Link>
+        <img src={logo} alt="Home Logo" className="App-logo" />
       <Link to="switch-account">Switch Accounts</Link>
       <div className="main-nav__user-actions">
         {isLoading ? (
@@ -18,17 +24,44 @@ export const Nav = () => {
         ) : (
           <>
             {!!currentUser && (
-              <span className="main-nav__username">
-                {getDisplayName(currentUser)}
-              </span>
+              <div className="group relative pb-1">
+                <div className="flex items-center cursor-pointer pb-1">
+                  <img 
+                    src={getProfilePictureUrl(currentUser)} 
+                    alt="Current User Profile" 
+                    className="w-10 h-10 rounded-full mr-2"
+                  />
+                  <span className="main-nav__username mr-2">
+                    {getDisplayName(currentUser)}
+                  </span>
+                </div>
+                <div className="absolute left-0 w-48 bg-gray-800 border border-gray-700 divide-y divide-gray-600 rounded-md shadow-lg z-50 group-hover:block hidden">
+                  {alternateUsers?.map((user) => (
+                    <div
+                      key={user.PublicKeyBase58Check}
+                      className="cursor-pointer px-4 py-2 text-white hover:bg-gray-700"
+                      onClick={() => identity.setActiveUser(user.PublicKeyBase58Check)}
+                    >
+                      <img 
+                        src={getProfilePictureUrl(user)} 
+                        alt="Alternate User Profile" 
+                        className="w-8 h-8 rounded-full mr-2 inline-block"
+                      />
+                      {getDisplayName(user)}
+                    </div>
+                  ))}
+                  <div className="cursor-pointer px-4 py-2 text-white hover:bg-gray-700" onClick={() => identity.login()}>
+                    Add another account
+                  </div>
+                  <div className="cursor-pointer px-4 py-2 text-white hover:bg-gray-700" onClick={() => identity.logout()}>
+                    Logout
+                  </div>
+                </div>
+              </div>
             )}
 
             {!currentUser && (
               <button onClick={() => identity.login()}>Login</button>
-            )}
-
-            {!!currentUser && (
-              <button onClick={() => identity.logout()}>Logout</button>
             )}
           </>
         )}
